feaa8758759395bf149bb0547e006bc64d9a7999
perpk <k.a.perperidis@gmail.com>
2022-02-18 09:22:27 +0200

    add a separator between temp dir and image tarball

--- src/main/java/co/pie/pie/util/Utils.java
+++ src/main/java/co/pie/pie/util/Utils.java
@@ -1,12 +1,24 @@
 package co.pie.pie.util;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+
 public class Utils {
-    public static String pushOrKeepLocally(boolean pushImage) {
-        if (pushImage) {
-            return "--push";
-        }
-        String tempDir = System.getProperty("java.io.tmpdir");
-        String keepLocally = String.format("-o type=tar,dest=%s%s", tempDir, "out.tar");
-        return keepLocally;
+  public static String pushOrKeepLocally(boolean pushImage) {
+    if (pushImage) {
+      return "--push";
     }
+    String tempDir = System.getProperty("java.io.tmpdir");
+    String keepLocally =
+        String.format("-o type=tar,dest=%s%s", addTrailingFileSeparator(tempDir), "out.tar");
+    return keepLocally;
+  }
+
+  public static String addTrailingFileSeparator(String dir) {
+    if (!StringUtils.endsWith(dir, File.separator)) {
+      return dir + File.separator;
+    }
+    return dir;
+  }
 }
